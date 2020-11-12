9fa626a6e6112d4a7a6a57a6e683cf8f393b5a53
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-12 16:27:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :email, uniqueness: true
+  validates :birthday, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
