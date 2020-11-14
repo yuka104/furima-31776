0ee70ce5c422ece1c79052536e80d380939cfb4c
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-14 20:06:42 +0900

    ActiveHashのconditionモデル作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,4 +12,7 @@ class Category < ActiveHash::Base
     { id: 10, name: 'ハンドメイド' },
     { id: 11, name: 'その他' }
   ]
+
+  include ActiveHash::Associations
+  has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+
+
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
     validates :price
     validates :state_id
     validates :delivery_days_id
