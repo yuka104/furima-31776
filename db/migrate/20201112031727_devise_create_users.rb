ec1fb6963f416624bf3fdbbcff2560f3ae616b97
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-14 15:58:18 +0900

    t.string :encrypted_passwordに変更

--- db/migrate/20201112031727_devise_create_users.rb
+++ db/migrate/20201112031727_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_kana,    null: false, default: ""
       t.date   :birthday,           null: false 
       t.string :email,              null: false, default: "", unique: true
-      t.string :password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
