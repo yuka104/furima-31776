7d84e58781af6a4841fb8f74aca0af75ffee10b6
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-14 20:20:11 +0900

    ActiveHashのstateモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :state
 
 
   with_options presence: true do
