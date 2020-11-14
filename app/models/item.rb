77538e5778edaeda523ab7fc24026facfa101a11
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-14 17:33:48 +0900

    Itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :price
+    validates :state_id
+    validates :delivery_days_id
+    validates :fee_id
+  end
+  
+  validates :user, foreign_key: true
 end
