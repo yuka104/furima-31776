93a90bdec1c430d0562f406f963efeba4524bbf6
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-15 18:15:04 +0900

    ActiveHushのview実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
 end
 
+private
+def prototype_params
+  params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :state_id, :delivery_days_id, :fee_id, :image).merge(user_id: current_user.id)
+end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
     validates :delivery_days_id, numericality: { other_than: 1 } 
     validates :fee_id, numericality: { other_than: 1 } 
   end
-  validates :user, foreign_key: true
+  
   belongs_to :user
 end
