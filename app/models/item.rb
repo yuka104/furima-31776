d90fb30fc5ec0cb487764042b685677864a4705f
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-18 08:45:30 +0900

    stateモデルの修正とitemモデルのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
+    validates :image
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
