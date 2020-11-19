dbb38e9d5c118fe0303f101e464861fa5d7e0761
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-19 15:01:07 +0900

    before_actionの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:edit, :update]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -25,10 +27,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to "/items.id"
+    else
+      render :edit
+    end
+  end
+
 
 private
 def item_params
   params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :state_id, :delivery_days_id, :fee_id, :image).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  @item = Item.find(params[:id])
+  unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to action: :index
+  end
 end
 
 end
