10012b3b49c18c11c841d5cecd9a15e3fdff473e
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-19 12:05:26 +0900

    編集ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :show, :edit] do
   end
 end
