dee69516cf8f6bf290d92f06d5f798e7b6c6257f
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-19 16:06:46 +0900

    before_action追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only:[:edit, :update]
   before_action :move_to_index, only: :edit
 
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end 
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -47,6 +45,10 @@ class ItemsController < ApplicationController
     unless user_signed_in? && current_user.id == @item.user.id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
