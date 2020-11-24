1b23004953eeff02f563137dc78a617286b10e6c
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-24 19:03:16 +0900

    購入済み商品のリダイレクト機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -37,7 +37,7 @@ class PurchasesController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if @item.purchase != nil
+    if @item.purchase != nil || current_user.id == @item.user.id
       redirect_to root_path
     end
   end
