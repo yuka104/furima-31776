29d838c1b34d7373008c2d1c5100e6c550366d8e
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-11-25 18:12:31 +0900

    購入済み商品機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id && @item.purchase == nil
       redirect_to action: :index
     end
   end
